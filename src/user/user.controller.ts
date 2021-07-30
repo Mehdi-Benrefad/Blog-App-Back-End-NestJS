b7a93588287e5d954af6cc978e8169b8f1a95b2b
Mehdi-Benrefad <benrefadmehdi@gmail.com>
2021-07-31 00:10:04 +0100

    Password Hashing, JWT authentication, Login EndPoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,7 +2,8 @@ import { Patch } from '@nestjs/common';
 import { Body, Delete, Get, Param, Post } from '@nestjs/common';
 import { Put } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Observable , of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { User } from './models/user.interface';
 import { UserService } from './user.service';
 
@@ -11,9 +12,26 @@ export class UserController {
     constructor(private userService: UserService) { }
 
     @Post()
-    create(@Body() user:User): Observable<User>{
-        return this.userService.create(user);
+    create(@Body() user:User): Observable<User | Object>{
+        return this.userService.create(user).pipe(
+            map((user: User) => user),
+            catchError(err => of({ error: err.message }))
+        );
+        //return this.userService.create(user);
     }
+
+
+    
+    @Post('login')
+    login(@Body() user: User): Observable<Object> {
+        return this.userService.login(user).pipe(
+            map((jwt: string) => {
+                return { access_token: jwt };
+            })
+        )
+    }
+
+
 
     @Get(':id')
     findOne(@Param()params:User): Observable<User>{
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,40 +1,132 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { from, Observable } from 'rxjs';
+import { from, Observable, throwError } from 'rxjs';
+import { AuthService } from 'src/auth/auth.service';
 import { Repository } from 'typeorm';
 import { UserEntity } from './models/user.entity';
 import { User } from './models/user.interface';
+import { switchMap, map, catchError} from 'rxjs/operators';
 
 @Injectable()
 export class UserService {
     constructor(
         @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>
+        ,private  authService:AuthService
     ) {}
 
 
 
     //foction de creation d'un utilisateur
     create(user: User): Observable<User> {
-        return from(this.userRepository.save(user));
+        //pipe():permet de combiner plusieurs fonctions 
+        return this.authService.hashPassword(user.password).pipe(
+            //switchMap():c'est un operateur qui nous permet de declancher des emissions de valeur, chaque fois qu'un autre observable emmet une valeur 
+            switchMap((passwordHash: string) => {
+                const newUser = new UserEntity();
+                newUser.name = user.name;
+                newUser.username = user.username;
+                newUser.email = user.email;
+                newUser.password = passwordHash;
+
+                return from(this.userRepository.save(newUser)).pipe(
+                    map((user: User) => {
+                        const {password, ...result} = user;
+                        return result;
+                    }),
+                    catchError(err => throwError(err))
+                )
+            })
+        )
+        //return from(this.userRepository.save(user));
     }
+
+
+
+
 
     //fonction qui retourne tous un utilisateur
     findOne(id:number): Observable<User>{
-        return from(this.userRepository.findOne(id));
+        return from(this.userRepository.findOne({id})).pipe(
+            map((user: User) => {
+                const {password, ...result} = user;
+                //console.log(result);
+                return result;
+            } )
+        )
+        //return from(this.userRepository.findOne(id));
     }
+
+
+
 
     //fonction qui retourne tous les utilisateurs
     findAll(): Observable<User[]>{
-        return from(this.userRepository.find());
+        return from(this.userRepository.find()).pipe(
+            //on supprime la proprete mot de passe de l'utilisateur.
+            map((users: User[]) => {
+                users.forEach(function (v) {delete v.password});
+                return users;
+            })
+        );
+        //return from(this.userRepository.find());
     }
+
+
+
 
     //fonction qui supprimme un utilisateurs
     deleteOne(id:number): Observable<any>{
         return from(this.userRepository.delete(id));
     }
 
+
+
+
     //fonctie qui modifie un utilisateur.
     updateOne(id:number , user:User): Observable<any>{
+        //on enleve le proprietes email et password de l'utilisateur.
+        delete user.email;
+        delete user.password;
         return from(this.userRepository.update(id , user));
+    }
+
+
+    //Login
+    login(user: User): Observable<string> {
+        return this.validateUser(user.email, user.password).pipe(
+            switchMap((user: User) => {
+                if(user) {
+                    return this.authService.generateJWT(user).pipe(map((jwt: string) => jwt));
+                } else {
+                    return 'Wrong Credentials';
+                }
+            })
+        )
+    }
+
+    
+    //Valider un utilisateurs
+    validateUser(email: string, password: string): Observable<User> {
+        return this.findByMail(email).pipe(
+            switchMap((user: User) => this.authService.comparePasswords(password, user.password).pipe(
+                map((match: boolean) => {
+                    if(match) {
+                        const {password, ...result} = user;
+                        console.log(result);
+                        return result;
+                    } else {
+                        throw Error;
+                    }
+                })
+            ))
+        )
+
+    }
+
+
+
+    //Rechercher l'utilisateur par Email.
+    findByMail(email: string): Observable<User> {
+        return from(this.userRepository.findOne({email}));
     }
 }
